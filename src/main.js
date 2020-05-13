2b615e3cf371e39d6d6799fb57496c3ade015bfe
Alexandru Nutu <alexnutu@gmail.com>
2020-05-13 12:51:37 -0700

    Added Vue-Material

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material';
 
 Vue.config.productionTip = false
+Vue.use(VueMaterial);
 
 new Vue({
   render: h => h(App),
