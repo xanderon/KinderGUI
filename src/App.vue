eee664b1c474697b4ce1dc1891df5bcf11eb6342
Alexandru Nutu <alexnutu@gmail.com>
2020-05-14 11:12:30 -0700

    Added Card List

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,15 @@
   <md-app md-waterfall md-mode="fixed">
     <my-navbar slot="md-app-toolbar"></my-navbar>
     <md-app-content>
-      <Card />
+      <Card
+        is="Card"
+        v-for="(chitanta) in chitanteMock"
+        v-bind:key="chitanta.date"
+        v-bind:name="chitanta.name"
+        v-bind:date="chitanta.date"
+        v-bind:amount="chitanta.amount"
+        v-bind:description="chitanta.description"
+      />
     </md-app-content>
   </md-app>
 </template>
@@ -12,6 +20,14 @@ import Card from "./components/Card.vue";
 
 export default {
   name: "app",
+  data() {
+    return {
+      chitanteMock: [
+      {name:'Leon', date:'20.10.2020', amount:'140 Lei', description:'TestD1'},
+      {name:'Mina', date:'20.10.2020', amount:'150 Lei', description:'TestD2'}            
+      ]
+    };
+  },
   components: {
     Card
   }
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,35 +1,21 @@
 <template>
   <div>
-    <md-card>
-      <md-card-header>
-        <div class="md-title">Card without hover effect</div>
-      </md-card-header>
-
-      <md-card-content>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-      </md-card-content>
-
-      <md-card-actions>
-        <md-button>Action</md-button>
-        <md-button>Action</md-button>
-      </md-card-actions>
-    </md-card>
-
     <md-card md-with-hover>
       <md-ripple>
         <md-card-header>
-          <div class="md-title">Card with hover effect</div>
-          <div class="md-subhead">It also have a ripple</div>
+          <div class="md-title">{{ amount }}</div>
+          <div class="md-subhead">{{ date }}</div>
+          <div class="md-subhead">{{ name }}</div>
         </md-card-header>
 
         <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+          {{ description }}
         </md-card-content>
 
-        <md-card-actions>
+        <!--<md-card-actions>
           <md-button>Action</md-button>
           <md-button>Action</md-button>
-        </md-card-actions>
+        </md-card-actions> -->
       </md-ripple>
     </md-card>
   </div>
@@ -46,6 +32,12 @@
 
 <script>
 export default {
-  name: 'RegularCards'
+  name: 'RegularCards',
+  props: [
+    "name",
+    "date",
+    "amount",
+    "description"
+  ],
 }
 </script>
