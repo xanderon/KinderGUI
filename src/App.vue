9aa5148822829adda9db0f6600440653a53db28c
Alexandru Nutu <alexnutu@gmail.com>
2020-05-14 12:44:47 -0700

    Added AddRecepit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <md-app md-waterfall md-mode="fixed">
     <my-navbar slot="md-app-toolbar"></my-navbar>
     <md-app-content>
+      <AddReceipt />
+      <md-card md-with-hover>
       <Card
         is="Card"
         v-for="(chitanta) in chitanteMock"
@@ -11,25 +13,43 @@
         v-bind:amount="chitanta.amount"
         v-bind:description="chitanta.description"
       />
+      </md-card>
+      <br />
+      <br />
+      <br />
+      <br />
+      | Version: KinderUI 1.0 |-| Build: 0.0.7 |
     </md-app-content>
   </md-app>
 </template>
 
 <script>
 import Card from "./components/Card.vue";
+import AddReceipt from "./components/AddReceipt.vue";
 
 export default {
   name: "app",
   data() {
     return {
       chitanteMock: [
-      {name:'Leon', date:'20.10.2020', amount:'140 Lei', description:'TestD1'},
-      {name:'Mina', date:'20.10.2020', amount:'150 Lei', description:'TestD2'}            
+        {
+          name: "Leon",
+          date: "20.10.2020",
+          amount: "140 Lei",
+          description: "TestD1"
+        },
+        {
+          name: "Mina",
+          date: "20.10.2020",
+          amount: "150 Lei",
+          description: "TestD2"
+        }
       ]
     };
   },
   components: {
-    Card
+    Card,
+    AddReceipt
   }
 };
 </script>
@@ -43,4 +63,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.md-card {
+    width: 380;
+    margin: 10px;
+    padding: 20px;
+    display: inline-block;
+    vertical-align: top;
+  }
 </style>
---
+++ src/components/AddReceipt.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <md-card md-with-hover>
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">Add Receipt</div>
+        </md-card-header>
+
+        <md-card-content>
+          <!-- Name Input -->
+          <md-field>
+            <label>Name</label>
+            <md-input v-model="vname"></md-input>
+          </md-field>
+          <!-- Amount Input -->
+          <md-field>
+            <label>Amount</label>
+            <md-input v-model="vamount"></md-input>
+          </md-field>
+          <!-- Date Input -->
+          <md-field>
+            <label>Date</label>
+            <md-input v-model="vdate"></md-input>
+          </md-field>
+          <!-- Description Input -->
+          <md-field>
+            <label>Description</label>
+            <md-input v-model="vdescription"></md-input>
+          </md-field>
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button>Add receipt</md-button>
+          <!-- <md-button>Reset Fields</md-button>-->
+        </md-card-actions>
+      </md-ripple>
+    </md-card>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.md-card {
+  width: 390px;
+  margin: 10px;
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
+
+<script>
+export default {
+  name: "AddReceipt",
+  props: ["name", "date", "amount", "description"],
+  data() {
+    return {
+      vname: "Name",
+      vdate: "Date",
+      vamount: "Amount",
+      vdescription: "Description"
+    };
+  },
+};
+</script>
